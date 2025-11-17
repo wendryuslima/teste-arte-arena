f651b29166aa1d462c83c8f0a0b70e9fd581b189
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2025-11-17 18:14:50 -0300

    feat: add componente shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import NavBar from "@/components/nav-bar";
 
-import { Button } from "@/components/ui/button";
-
 const Home = () => {
   return (
     <>
-    <NavBar/>
+      <NavBar />
       <div className="flex flex-col space-y-6 overflow-hidden p-10 lg:overflow-hidden">
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-2">
           <h1 className="text-2xl font-bold">Dashboard</h1>
+          <h1 className="text-sm font-bold text-gray-400">Finance App</h1>
         </div>
         <div className="flex flex-col items-center justify-between">
           <div className="flex items-center gap-3"></div>
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -5,41 +5,38 @@ import { usePathname } from "next/navigation";
 const NavBar = () => {
   const pathname = usePathname();
   return (
-    <nav className="flex flex-col items-center justify-between border-b border-solid px-4 py-4 lg:flex-row lg:px-8 lg:py-4">
-      <div className="relative h-10 w-32 lg:h-10 lg:w-44"></div>
-      <div className="flex w-full items-center gap-2 px-2 lg:gap-10">
-        <div className="lg:pr-auto flex w-full items-center gap-6 lg:gap-10">
-          <Link
-            href="/"
-            className={`text-sm lg:text-xl ${
-              pathname === "/"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }`}
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="/transactions"
-            className={`text-sm lg:text-xl ${
-              pathname === "/transactions"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }`}
-          >
-            Transações
-          </Link>
-          <Link
-            href="/subscription"
-            className={`text-sm lg:text-xl ${
-              pathname === "/subscription"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }`}
-          >
-            Assinatura
-          </Link>
-        </div>
+    <nav className="flex flex-col items-center justify-center border-b border-solid px-6 py-8 lg:flex-row lg:px-12 lg:py-6">
+      <div className="flex items-center justify-center gap-8 lg:gap-12">
+        <Link
+          href="/"
+          className={`text-base lg:text-xl ${
+            pathname === "/"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }`}
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/transactions"
+          className={`text-base lg:text-xl ${
+            pathname === "/transactions"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }`}
+        >
+          Transações
+        </Link>
+        <Link
+          href="/subscription"
+          className={`text-base lg:text-xl ${
+            pathname === "/subscription"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }`}
+        >
+          Assinatura
+        </Link>
       </div>
     </nav>
   );
